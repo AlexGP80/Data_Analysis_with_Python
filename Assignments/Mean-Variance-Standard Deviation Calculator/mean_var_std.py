ad612b5c4c2d9371e33c11d758936d012b3b0396
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-04-11 19:15:05 +0200

    Data Analysis with Python - Assignment 1

--- Assignments/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Assignments/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -2,24 +2,24 @@ import numpy as np
 
 def calculate(list):
   if len(list) != 9:
-    raise ValueError("List must contain nine numbers")
+    raise ValueError("List must contain nine numbers.")
 
   matrix = np.array(list).reshape((3, 3))
 
   # print(matrix)
-  
+
   calculations = {}
-  calculations['mean'] = [matrix.mean(axis=0), matrix.mean(axis=1), matrix.mean()]
-  calculations['variance'] = [matrix.var(axis=0), matrix.var(axis=1), matrix.var()]
-  calculations['standard deviation'] = [matrix.std(axis=0), matrix.std(axis=1), matrix.std()]
-  calculations['max'] = [matrix.max(axis=0), matrix.max(axis=1), matrix.max()]
-  calculations['min'] = [matrix.min(axis=0), matrix.min(axis=1), matrix.min()]
-  calculations['sum'] = [matrix.sum(axis=0), matrix.sum(axis=1), matrix.sum()]
-  
+  calculations['mean'] = [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean()]
+  calculations['variance'] = [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var()]
+  calculations['standard deviation'] = [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std()]
+  calculations['max'] = [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max()]
+  calculations['min'] = [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min()]
+  calculations['sum'] = [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum()]
+
   # print(calculations)
 
-  
-  
+
+
 
 
 
